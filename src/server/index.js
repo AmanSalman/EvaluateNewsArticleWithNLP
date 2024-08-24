be4b03d1494f62e6cebde54cfd9e4c7c709239f4
AmanSalman <rapamansalman@gmail.com>
2024-08-24 20:40:45 +0300

    changes .env.PORT

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const path = require('path');
 const cors = require('cors');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config()
 
 
 const app = express();
@@ -10,13 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../../dist')));
-dotenv.config();
 
-const port = 4000;
+const port = process.env.PORT;
 
 
 const API_KEY = process.env.MEANINGCLOUD_API_KEY;
-
+console.log(API_KEY)
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../dist/index.html'));
 });
